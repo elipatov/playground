b4b5da6b4fccc0a5502a0b9a11b1db6beee596dc
Evgeni Lipatov <yauheni.lipatov@adform.com>
2019-07-10 19:26:21 +0300

    Reading stream

--- src/main/scala/example/WordCountApplication.scala
+++ src/main/scala/example/WordCountApplication.scala
@@ -1,12 +1,19 @@
+package example
+
+import java.time.Duration
 import java.util.Properties
 import java.util.concurrent.TimeUnit
 
-import org.apache.kafka.streams.kstream.{KStream, KTable, Materialized}
-import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
+import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.kstream.Materialized
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.common.serialization.Serdes._
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.streams.scala._
+import org.apache.kafka.streams.scala.kstream._
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 
 object WordCountApplication extends App {
-  import org.apache.kafka.common.serialization.Serdes._
-
   val props: Properties = {
     val p = new Properties()
     p.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application")
@@ -15,17 +22,17 @@ object WordCountApplication extends App {
   }
 
   val builder: StreamsBuilder = new StreamsBuilder
-  val textLines: KStream[String, String] = builder.stream[String, String]("TextLinesTopic")
-  val wordCounts: KTable[String, Long] = textLines
-    .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
-    .groupBy((_, word) => word)
-    .count()(Materialized.as("counts-store"))
-  wordCounts.toStream.to("WordsWithCountsTopic")
+  val textLines: KStream[String, String] = builder.stream[String, String]("TextLinesTopic")//(ImplicitConversions.consumedFromSerde[String, String])
+//  val wordCounts: KTable[String, Long] = textLines
+//    .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
+//    .groupBy((_, word) => word)
+//    .count()(Materialized.as("counts-store"))
+//  wordCounts.toStream.to("WordsWithCountsTopic")
+//
+//  val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
+//  streams.start()
 
-  val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
-  streams.start()
-
-  sys.ShutdownHookThread {
-    streams.close(10, TimeUnit.SECONDS)
-  }
+//  sys.ShutdownHookThread {
+//    streams.close(Duration.ofSeconds(10))
+//  }
 }
